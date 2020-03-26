98003828d1ed147887d9fe0b9485959c9f9b7767
HanenFourati <fouratihanen@gmail.com>
2020-03-26 20:23:10 +0100

    Updates

---
+++ precache-manifest.c9e1f93df589c46443c63491ba1d90fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbcc5c697d372b3a080f84541c7a871",
+    "url": "/usinghooks/index.html"
+  },
+  {
+    "revision": "877537fcdb31b75509b7",
+    "url": "/usinghooks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2e0b8173083e3e847af",
+    "url": "/usinghooks/static/js/2.cddd5dbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/usinghooks/static/js/2.cddd5dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877537fcdb31b75509b7",
+    "url": "/usinghooks/static/js/main.aca4ff5f.chunk.js"
+  },
+  {
+    "revision": "549a0de78e5677d4f0e3",
+    "url": "/usinghooks/static/js/runtime-main.da0656e9.js"
+  }
+]);
